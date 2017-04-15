8c2c3722b153f4ec790232b41c362ba7cca24f5e
Witold <blue3656@gmail.com>
2017-04-16 00:01:31 +0100

    understanding algo nb 7

--- advancedAlgos.js
+++ advancedAlgos.js
@@ -398,4 +398,70 @@ function permAlone(str) {
 
 permAlone('abfdefa');
 
-// -------------------  7 ----- 
+// -------------------  7 -----  Recursive function that generates permutations of a string
+
+
+function permAlone(string) {
+  
+ function permut(string) {
+    if (string.length < 2) return string; // This is our break condition
+
+    var permutations = []; // This array will hold our permutations
+
+    for (var i=0; i<string.length; i++) {
+        var char = string[i];
+
+        // Cause we don't want any duplicates:
+        if (string.indexOf(char) != i) // if char was used already
+            continue;           // skip it this time
+
+        var remainingString = string.slice(0,i) + string.slice(i+1,string.length); //Note: you can concat Strings via '+' in JS
+
+        for (var subPermutation of permut(remainingString))
+            permutations.push(char + subPermutation);
+
+    }
+    return permutations;
+}
+
+var permutations = permut(string);
+for (permutation of permutations)
+    console.log(permutation); //Use the output method of your choice
+  
+  /*
+  function permute(str){
+    
+    let arr =[];   
+    let l = str.length;
+    
+    if(l<2){return str;}
+    
+    for(var i=0; i<l; i++){
+      
+      let c = str[i];
+      
+      if (str.indexOf(c) != i) // if char was used already
+            continue;           // skip it this time
+
+      let strSlice = str.slice(0,i) + (i+1, l);
+      
+      for(var x of permute(strSlice)){
+        
+        arr.push(c + x);
+        console.log(arr);
+      }
+      
+      
+    }
+    return arr;
+    
+  }
+   
+  for(x of permute(str)){
+    console.log(x);
+  }
+}
+*/
+} 
+permAlone('aab');
+
